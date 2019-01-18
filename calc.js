aebe6df67ee213a4093b9453ea5078e393349f7a
Elias Chacin <elichaci@publicisgroupe.net>
2019-01-18 14:53:23 -0500

    refactored code

--- calc.js
+++ calc.js
@@ -1,67 +1,122 @@
-'use strict';
-var str = '';
-var parsed_number;
-var stored_number = null;
+"use strict";
+
+var str = "";
+var parsed_number = null;
+var buffer = null;
 var op = null;
-var field = $('input');
+var field = $("input");
 var clear = false;
 
-$('button').click(function (e) {
+//-----FLAGS-----//
+var op_flag = false;
+var equals_flag = false;
+var num_flag = false;
+
+$("button").click(function (e) {
     var elemClicked = e.target;
     var idClicked = e.target.id;
     var classClicked = e.target.className;
 
-    str += elemClicked.value;
-    parsed_number = parseInt(str);
+    //---------IF NON-NUMBER CLICKED------------//
+    if (classClicked === "non-num") {
 
-    if (classClicked === 'non-num') { //IF NON-NUMBER CLICKED
+        if (str) {
+            parsed_number = parseInt(str);
+        }
         str = '';
         clear = true;
 
-        if (idClicked === 'clearButton') {
-            op = null;
-            stored_number = null;
-            field.val('');
-
-        } else if (idClicked == 'equalsButton') {
-
-            switch (op) {
-                case 'addButton':
-                    stored_number = stored_number + parsed_number;
-                    field.val(stored_number);
-                    break;
-
-                case 'subtractButton':
-                    stored_number = stored_number - parsed_number;
-                    field.val(stored_number);
-                    break;
-
-                case 'multiplyButton':
-                    stored_number = stored_number * parsed_number;
-                    field.val(stored_number);
-                    break;
-
-                case 'divideButton':
-                    stored_number = stored_number / parsed_number;
-                    field.val(stored_number);
-                    break;
-            }
-        } else {
-            op = idClicked;
-            if(parsed_number){
-                stored_number = parsed_number;
-            }
+        if (idClicked === "clearButton") {//CLEAR
+            clearAll();
         }
-    } else { //IF NUMBER CLICKED
+
+        else if (idClicked == "equalsButton") {//EQUALS
+
+            operate(buffer, parsed_number, op);
+            showResult();
+
+            equals_flag = true;
+        }
+
+        else {//OPERATIONS
+
+            if (op_flag) {
+
+                if (num_flag && !equals_flag) {
+                    operate(buffer, parsed_number, op);
+                    showResult();
+                } else {
+                    equals_flag = false;
+                }
+            } else {
+                buffer = parsed_number;
+                op_flag = true;
+            }
+            op = idClicked;
+        }
+        num_flag = false;
+    }
+
+    //-----------------IF NUMBER CLICKED----------------//
+    else {
+        str += elemClicked.value;
+
         if (clear) {
-            field.val('');
+            field.val("");
             clear = false;
         }
         field.val(field.val() + $(elemClicked).val());
+        num_flag = true;
     }
 
     //DEBUG
-    $('#output1').html('operation: ' + op);
-    $('#output2').html('stored: ' + stored_number);
+    $("#output1").html("operation: " + op);
+    $("#output2").html("buffer: " + buffer);
+    $("#output7").html("str: " + str);
+    $("#output3").html("parsed_number: " + parsed_number);
+    $("#output4").html("num_flag: " + num_flag);
+    $("#output5").html("op_flag: " + op_flag);
+    $("#output6").html("equals_flag: " + equals_flag);
 });
 
+function clearAll() {
+    op = null;
+    buffer = null;
+    parsed_number = null;
+    field.val("");
+    op_flag = false;
+    equals_flag = false;
+    num_flag = false;
+}
+
+function operate(a, b, o) {
+    switch (o) {
+        case "addButton":
+            buffer = a + b;
+            break;
+
+        case "subtractButton":
+            buffer = a - b;
+            break;
+
+        case "multiplyButton":
+            buffer = a * b;
+            break;
+
+        case "divideButton":
+            buffer = a / b;
+            break;
+    }
+}
+
+function showResult() {
+    field.val(buffer);
+}
+
+
+//EQUALS BACKUP
+// if (num_flag && !equals_flag && op_flag) {
+//     operate(buffer, parsed_number, op);
+//     showResult();
+//     equals_flag = true;
+// }
